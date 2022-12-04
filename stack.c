551e1028c1ba1cfcffe9c11b32b3d4d1b0662ae2
Aarreboy <117940009+Aarreboy@users.noreply.github.com>
2022-12-04 11:17:01 +0100

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,30 +1,66 @@
+/*
+ * Programmering i C
+ * Fall 22
+ * OU4
+ *
+ * @file             stack.c
+ * @brief            Defines the functions from "stack.h".
+ *                  
+ * @author           Aaron Myhrberg (ens21amg)
+ * @defgroup         stack.c
+ * @since            2022-11-30
+ */
+
 #include <stdio.h>
 #include "stack.h"
 #include <stdlib.h>
+
 
 // Create an empty stack.
 
 Stack *stack_create(void)
 {
     Stack *p = malloc(sizeof(Stack));
-    //p.size = ?
-    //p->capacity = ?
-    p->data = malloc(p->size * sizeof(double));
+    p->size = 0;
+    p->capacity = 1; 
+    p->data = malloc(p->capacity * sizeof(double));
 
+    return p;
 }
 
 // Destroy the stack.
-void stack_destroy(Stack *s);
+void stack_destroy(Stack *s)
+{
+    free(s->data);
+    free(s);
+}
 
 // Push a value onto the stack.
 void stack_push(Stack *s, double value)
 {
-    //kapaciteten dubbleras när size == capacity 
-}
+    if (s->size == s->capacity){
+        s->data = realloc(s->data, s->capacity * 2);
+    }
+    s->data[s->size] = value;
+    s->size++;
+} 
 
 // Pop the value at the top of the stack.
-double stack_pop(Stack *s);
+double stack_pop(Stack *s)
+{
+    double res = s->data[s->size - 1];
+    s->size--;
+    return res;
+}
+
 
 // Check if the stack is empty.
-bool stack_is_empty(const Stack *s);
-
+bool stack_is_empty(const Stack *s)
+{
+    if (s->size == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
