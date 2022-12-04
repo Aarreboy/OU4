b197f41ac0659da4302b01d61587c8424a696d40
Aarreboy <117940009+Aarreboy@users.noreply.github.com>
2022-12-04 16:48:52 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -4,7 +4,7 @@
 #include "list.h"
 #include <string.h>
 
-#define SIZE 20
+#define SIZE 30
 
 Queue *queue_create(void)
 {
@@ -14,28 +14,22 @@ Queue *queue_create(void)
     return q;
 }
 
-
 void queue_destroy(Queue *q)
 {
     list_destroy(q->list);
     free(q);
 }
 
-
 void queue_enqueue(Queue *q, const char *value)
 {
     list_insert(list_end(q->list), value);
 }
   
-
-
-
-
-
 char *queue_dequeue(Queue *q)
 {    
     char *result; 
     result = malloc(sizeof(char[SIZE]));
+    
     ListPos firstpos = list_first(q->list);
     strncpy(result, list_inspect(firstpos), SIZE);
     list_remove(firstpos);
@@ -48,3 +42,4 @@ bool queue_is_empty(const Queue *q)
 
     return list_is_empty(q->list);
 }
+
